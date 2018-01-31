cc0df918bbf8012ee111882bd9c810db0cb71771
nikolaCh6 <nikolachmiel6@gmail.com>
2018-01-31 08:45:24 +0100

    webmaster

---
+++ publlic_html/php/index.php
@@ -0,0 +1,6 @@
+<?php
+// miejsce na kod PHP
+    echo("Witaj na mojej stronie");
+    //zainstaluj server
+    paiton welocome to painto sciągnij painton 364 wersje stabilną executable instaler python 3 6 4 
+?>
