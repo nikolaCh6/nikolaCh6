949c0fd45d0ee95e6704f8164ebce333b9185737
nikolaCh6 <nikolachmiel6@gmail.com>
2019-01-04 08:45:01 +0100

    NOWE REPO

--- publlic_html/php/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// miejsce na kod PHP
-    echo("Witaj na mojej stronie");
-    //zainstaluj server
-    paiton welocome to painto sciągnij painton 364 wersje stabilną executable instaler python 3 6 4 
-?>
