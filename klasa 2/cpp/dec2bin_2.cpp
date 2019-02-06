8a32da28d00246c559e5d7505f8492d3f17104d2
nikolaCh6 <nikolachmiel6@gmail.com>
2019-02-06 11:20:21 +0100

    mati

--- klasa 2/cpp/dec2bin_2.cpp
+++ klasa 2/cpp/dec2bin_2.cpp
@@ -5,7 +5,11 @@
 
 
 #include <iostream>
+#include <cmath>
+// pow(liczba, n);
 using namespace std;
+
+int cyfry[16] = {0,1,2,3,4,5,6,7,8,9,65,66,67,68,69,70};
 
 int dec2any(int liczba, int podstawa, int tab[])
 {
@@ -20,7 +24,29 @@ int dec2any(int liczba, int podstawa, int tab[])
 
 void bin2dec(int tab[])
 {
-    ;
+    int podstawa = 0;
+    do {
+        cout << "Podstawa [2;9]: ";
+        cin >> podstawa;
+    } while (podstawa < 2 || podstawa > 9);
+    
+    int ile = 0;
+    cout << "Ile cyfr? ";
+    cin >> ile;
+    for(int i = 0; i < ile; i++)
+        do{
+            cout << "Podaj cyfre [0-" << podstawa-1 << "]: ";
+            cin >> tab[i];
+        }while(tab[i] < 0 || tab[i] > podstawa-1);
+        
+    //KONWERSJA JUST BEGIN
+    int liczba10 = 0;
+    for(int i = 0; i < ile ; i++)
+    {
+        liczba10 += tab[i] * pow(podstawa, ile-1-i);
+    }
+    cout << "Wynik: "<< liczba10;
+   
 }
 
 int main(int argc, char **argv)
@@ -31,10 +57,12 @@ int main(int argc, char **argv)
     cin >> liczba >> podstawa;
     int i = dec2any(liczba, podstawa, tab);
     cout << "Wynik: ";
-    while (i > -1) {
-        cout << tab[i];
-        i--;
-        } 
+    while (i > -1) 
+    {
+       if(podstawa)
+    } 
+    cout << endl;
+    bin2dec(tab);
 	return 0;
 }
 
