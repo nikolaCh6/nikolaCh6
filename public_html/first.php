001e4889f3d613c48d49814892470c9ba112baf7
nikolaCh6 <nikolachmiel6@gmail.com>
2019-03-15 07:14:31 +0100

    php dla Bananka

---
+++ public_html/first.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<!-- first.php -->
+<head>
+    <meta charset="utf-8" />
+    <title>Test PHP</title>
+<?php
+
+?>
+</head>
+
+<body>
+<?php
+    echo("<h1>Witaj w PHP!</h1>");
+?>
+</body>
+
+</html>
---
+++ public_html/inc/template.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo $tytul; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="static/css/scrolling-nav.css" rel="stylesheet">
+    <link href="static/css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="{{ url_for('index') }}">Strona główna</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <?php
+                get_menu($id);
+            ?>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="bg-primary text-white">
+      <div class="container text-center">
+        <h1>Aplikacja PHP</h1>
+        <p class="lead">Moja pierwsza aplikacja w PHP</p>
+      </div>
+    </header>
+
+    <section id="quiz">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <?php
+                get_koms();
+                get_page_content($id);
+            ?>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="static/vendor/jquery/jquery.min.js"></script>
+    <script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="static/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="js/scrolling-nav.js"></script>
+
+  </body>
+
+</html>
